22b8c22dca662a8b0f09f3f30e0ba46916aef409
PabloDerRohe <pablomarelli2@gmail.com>
2022-04-18 22:08:35 -0300

    calculadora agregada y retoques finales

--- calculator/models.py
+++ calculator/models.py
@@ -1,19 +1,26 @@
+from ast import Pass
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
 
-class CalculadoraInteres(models.Model):
-        
+class Calculadora(models.Model):
     nombre = models.CharField(max_length=30)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    monto_inicial = models.DecimalField(max_digits=30, decimal_places=2)
-    ahorro_mensual = models.DecimalField(max_digits=30, decimal_places=2)
-    interes = models.DecimalField(max_digits=30, decimal_places=2)
-    acumulado_con_interes = models.DecimalField(max_digits=30, decimal_places=2, null=True, blank=True)
-    acumulado_sin_interes = models.DecimalField(max_digits=30, decimal_places=2, null=True, blank=True)
-    año = models.IntegerField(null=True, blank=True)
+    capital_inicial = models.DecimalField(max_digits=50, decimal_places=2)
+    capital_adicional = models.DecimalField(max_digits=50, decimal_places=2)
+    interes = models.DecimalField(max_digits=50, decimal_places=2)
+    años = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.id} | {self.nombre}'
+
+class CapitalTiempo(models.Model):
+    year = models.IntegerField()
+    capital = models.DecimalField(max_digits=50, decimal_places=2)
+    calc = models.ForeignKey(Calculadora, on_delete=models.CASCADE, related_name='capitaltiempo')
     
     def __str__(self):
-        return f'{self.nombre}'
+        return f'{self.calc} | {self.year} = {self.capital}'
+
--- calculator/views.py
+++ calculator/views.py
@@ -1,43 +1,101 @@
+from django.forms import DecimalField
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
-from .models import CalculadoraInteres
-from calculator.forms import CalculatorForm
+
+from .models import Calculadora, CapitalTiempo
+from .forms import CalculatorForm
+from .utils import get_plot
+
+from django.contrib.auth.decorators import login_required
+
+from django.db.models import Max
 
 # Create your views here.
 
 
-def calculator(request):
+@login_required
+def list_calculator(request):
+    
+    calc = Calculadora.objects.all()
+    
+    data = {
+        'calc': calc,
+    }
+    
+    return render(request, 'calculator/list_calculator.html', data)
+
+    
+@login_required
+def create_calculator(request):
     
     if request.method == 'POST':
-        form = CalculatorForm(request.POST, request.FILES)
+        form = CalculatorForm(request.POST)
 
         if form.is_valid():
             data = form.cleaned_data
             
-            i=0
-            monto = data['monto_inicial']
-            
-            while i < data['año']:
-                monto = (monto+(data['ahorro_mensual']*12))*(1+data['interes']*0.01)
-                i = i + 1
-                calculo_con_interes = {i: monto}
-                
-            
-            calculo_sin_interes = data['monto_inicial']+((data['ahorro_mensual']*12)*data['año'])
-            
-            nuevo_post = CalculadoraInteres(
+            nuevo_calculo = Calculadora(
                 nombre=data['nombre'],
-                usuario=data['usuario'],
-                monto_inicial=data['monto_inicial'],
-                ahorro_mensual=data['ahorro_mensual'], 
+                usuario=request.user,
+                capital_inicial=data['capital_inicial'],
+                capital_adicional=data['capital_adicional'], 
                 interes=data['interes'],
-                acumulado_con_interes=calculo_con_interes,
-                acumulado_sin_interes=calculo_sin_interes,
-                año=data['año'],
+                años=data['años'],
                 )
+            nuevo_calculo.save()
             
-            nuevo_post.save()
-            return redirect('listado_posts')
+            calculadora = Calculadora.objects.filter(id=nuevo_calculo.id).values()
+            datos = calculadora[0]
+            monto = float(datos['capital_inicial'])
+            adicional = float(datos['capital_adicional'])
+            interes = float(datos['interes'])
+            años = int(datos['años'])
+                
+            for año in range(1, años):
+                monto = (monto+(adicional*12))*(1 + interes*0.01)
+                capital_tiempo = CapitalTiempo(
+                        year = año,
+                        capital = monto,
+                        calc = Calculadora.objects.get(id=nuevo_calculo.id)
+                    )
+                capital_tiempo.save()
             
+            # calculo_sin_interes = data['monto_inicial']+((data['ahorro_mensual']*12)*data['año'])
+            return redirect('list_calculator')
+    
     form = CalculatorForm()
-    return render(request, 'calculator/calculator.html', {'form': form})
+    
+    
+    data = {
+        'form': form,
+    }
+    return render(request, 'calculator/create_calculator.html', data)    
+
+
+@login_required
+def graph(request, id):
+    
+       
+    qs = CapitalTiempo.objects.filter(calc=id)
+    x = [x.year for x in qs]
+    y = [y.capital for y in qs]
+    chart = get_plot(x,y)
+    
+    capital_max = qs.aggregate(Max('capital'))
+    capital = capital_max['capital__max']
+    
+    data = {
+        'chart': chart,
+        'capital': capital
+    }
+    return render(request, 'calculator/graph.html', data)  
+
+
+@login_required
+def delete_calculator(request, id):
+    
+    calc = Calculadora.objects.get(id=id)    
+    calc.delete()
+    
+    return redirect('list_calculator')
